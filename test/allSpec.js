16f940bd4b1120033dcb31e7ea1f1088ae1893de
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-11-08 14:00:29 +0000

    adding .all and .race to the tests.

--- test/allSpec.js
+++ test/allSpec.js
@@ -2,4 +2,60 @@ var promise = require('../lib/index');
 
 describe(".ALL", function () {
 
+    describe("invalid input parameter", function () {
+
+        it("must throw an error", function () {
+            var values = [undefined, null, 0, 123, true];
+            values.forEach(function (v) {
+                {
+                    expect(function () {
+                        promise.all(v);
+                    }).toThrow("You must pass an array to Promise.all().");
+                }
+            });
+        });
+
+    });
+
+    describe("empty input", function () {
+        var result;
+        beforeEach(function (done) {
+            promise.all([])
+                .then(function (data) {
+                    result = data;
+                    done();
+                });
+        });
+        it("must resolve with empty result", function () {
+            expect(result).toEqual([]);
+        });
+    })
+
+    describe("simple values input", function () {
+        var result;
+        beforeEach(function (done) {
+            promise.all([1, 2, 3])
+                .then(function (data) {
+                    result = data;
+                    done();
+                });
+        });
+        it("must resolve with simple values", function () {
+            expect(result).toEqual([1, 2, 3]);
+        });
+    })
+
+    describe("promise values input", function () {
+        var result;
+        beforeEach(function (done) {
+            promise.all([promise.resolve(1), promise.resolve(2), promise.resolve(3)])
+                .then(function (data) {
+                    result = data;
+                    done();
+                });
+        });
+        it("must resolve with the resolved values", function () {
+            expect(result).toEqual([1, 2, 3]);
+        });
+    })
 });
--- test/raceSpec.js
+++ test/raceSpec.js
@@ -2,4 +2,47 @@ var promise = require('../lib/index');
 
 describe(".RACE", function () {
 
+    describe("invalid input parameter", function () {
+
+        it("must throw an error", function () {
+            var values = [undefined, null, 0, 123, true];
+            values.forEach(function (v) {
+                {
+                    expect(function () {
+                        promise.race(v);
+                    }).toThrow("You must pass an array to Promise.race().");
+                }
+            });
+        });
+
+    });
+
+    describe("simple values input", function () {
+        var result;
+        beforeEach(function (done) {
+            promise.race([1, 2, 3])
+                .then(function (data) {
+                    result = data;
+                    done();
+                });
+        });
+        it("must resolve with the first value", function () {
+            expect(result).toBe(1);
+        });
+    })
+
+    describe("promise values input", function () {
+        var result;
+        beforeEach(function (done) {
+            promise.race([promise.resolve(1), promise.resolve(2), promise.resolve(3)])
+                .then(function (data) {
+                    result = data;
+                    done();
+                });
+        });
+        it("must resolve with the first resolved value", function () {
+            expect(result).toBe(1);
+        });
+    })
+
 });
